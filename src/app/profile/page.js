63949c64167d9a348a3b14dec43160b07734cdc4
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2023-11-15 17:02:57 -0500

    Auth - Supabase

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import Link from 'next/link';
@@ -6,7 +5,7 @@ import { redirect } from 'next/navigation';
 
 import SignOut from 'src/components/SignOut';
 
-export default async function Home() {
+export default async function Profile() {
   const supabase = createServerComponentClient({ cookies });
 
   const {
@@ -19,10 +18,12 @@ export default async function Home() {
 
   return (
     <div className="card">
-      <h2>Welcome!</h2>
-      <code className="highlight">{user.role}</code>
-      <Link className="button" href="/profile">
-        Go to Profile
+      <h2>User Profile</h2>
+      <code className="highlight">{user.email}</code>
+      <div className="heading">Last Signed In:</div>
+      <code className="highlight">{new Date(user.last_sign_in_at).toUTCString()}</code>
+      <Link className="button" href="/">
+        Go Home
       </Link>
       <SignOut />
     </div>
